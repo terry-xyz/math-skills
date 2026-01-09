dc76a7c0a0609de87ae4b8b65d46defc1f36b762
lpapanthy <deezolut@gmail.com>
2026-01-09 16:19:14 +0200

    feat: add file reader to parse numbers into slice

---
+++ cmd/math-skills/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	numbers := readContent("../../data.txt")
+	fmt.Println("Numbers: ", numbers)
+}
+
+func readContent(path string) []int {
+	// read the file content
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+		os.Exit(1)
+	}
+	// split the content into lines
+	lines := strings.Split(string(content), "\n")
+	// create a slice of integers
+	numbers := []int{}
+	for _, line := range lines {
+		line = strings.TrimSpace(line) // trim the space from the line
+		if line == "" {
+			continue // skip the empty line
+		}
+		num, err := strconv.Atoi(line) // convert the line to an integer
+		if err != nil {
+			log.Fatalf("failed to convert string to int: %v", err)
+			os.Exit(1)
+		}
+		numbers = append(numbers, num)
+	}
+	// return the numbers as a slice of integers
+	return numbers
+}
